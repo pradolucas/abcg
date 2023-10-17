ea4d3a65d06f0f0c619e87d05663f17183cce110
karine <karine.silva.sobrinho@gmail.com>
2023-10-17 17:26:58 -0300

    changing the window name

--- examples/geometry_generator/main.cpp
+++ examples/geometry_generator/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
     window.setWindowSettings({
         .width = 900,
         .height = 900,
-        .title = "Projeto 1",
+        .title = "Geometry generator",
     });
 
     app.run(window);
